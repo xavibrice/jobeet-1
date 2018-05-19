4e874fd62c63da4d25ee56af1f1cc7b44991f3e9
Gregurco Vlad <gregurco.vlad@gmail.com>
2018-05-19 09:46:40 +0300

    Improve tests

--- tests/Controller/JobControllerTest.php
+++ tests/Controller/JobControllerTest.php
@@ -3,16 +3,18 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class JobControllerTest extends WebTestCase
 {
-    public function testListAction()
+    public function testList()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
 
         $this->assertEquals(
             1,
@@ -20,13 +22,13 @@ class JobControllerTest extends WebTestCase
         );
     }
 
-    public function testCreateAction()
+    public function testCreate()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/job/create');
+        $crawler = $client->request(Request::METHOD_GET, '/job/create');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
 
         $form = $crawler->filter('button:contains("Create")')->form();
 
@@ -45,5 +47,29 @@ class JobControllerTest extends WebTestCase
         $this->assertTrue($client->getResponse()->isRedirect());
 
         $this->assertStringStartsWith('/job/', $client->getResponse()->headers->get('location'));
+    }
+
+    public function testCreateWithoutEmail()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request(Request::METHOD_GET, '/job/create');
+
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+
+        $form = $crawler->filter('button:contains("Create")')->form();
+
+        $form['job[type]'] = 'part-time';
+        $form['job[company]'] = 'Pentalog';
+        $form['job[position]'] = 'Developer';
+        $form['job[location]'] = 'Chisinau';
+        $form['job[description]'] = 'some long long description';
+        $form['job[howToApply]'] = 'email';
+
+        $crawler = $client->submit($form);
+
+        $this->assertEquals(1, $crawler->filter('.has-error')->count());
+
+        $this->assertFalse($client->getResponse()->isRedirect());
     }
 }
